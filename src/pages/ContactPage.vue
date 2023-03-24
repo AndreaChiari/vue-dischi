5eb824fb11aba3918e9c1c45176a959197221d25
SamuelePassiatore <semmisno2@protonmail.com>
2023-03-24 19:03:56 +0100

    added send form with empty form method

--- src/pages/ContactPage.vue
+++ src/pages/ContactPage.vue
@@ -1,6 +1,24 @@
 <script>
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/contact_message';
+const emptyForm = { email: '', subject: '', message: '', subscribe: false };
 export default {
   name: "ContactPage",
+  data: () => ({
+    form: emptyForm
+  }),
+  methods: {
+    sendForm() {
+      axios.post(endpoint, this.form)
+        .then(() => {
+          this.form = { email: '', subject: '', message: '', subscribe: false };
+          this.emptyForm();
+        })
+    },
+    emptyForm() {
+      this.form = { email: '', subject: '', message: '', subscribe: false };
+    }
+  }
 };
 </script>
 
@@ -8,23 +26,25 @@ export default {
   <div class="container">
     <h1 class="my-4">Contacts</h1>
 
-    <form novalidate>
+    <form @submit.prevent="sendForm" novalidate>
       <div class="row">
         <div class="mb-3 col-6">
           <label for="email" class="form-label">Email address<sup class="text-danger">*</sup></label>
-          <input type="email" class="form-control" id="email" name="email" />
+          <input v-model.trim="form.email" type="email" class="form-control" id="email" name="email" required />
         </div>
         <div class="mb-3 col-6">
           <label for="subject" class="form-label">Subject<sup class="text-danger">*</sup></label>
-          <input type="email" class="form-control" name="subject" />
+          <input v-model.trim="form.subject" type="text" class="form-control" id="subject" name="subject" required />
         </div>
         <div class="mb-3 col-12">
           <label class="form-label">Text Us!<sup class="text-danger">*</sup></label>
-          <textarea class="col-12 form-control" name="" id=""></textarea>
+          <textarea v-model.trim="form.message" class="col-12 form-control" name="message" id="message"
+            required></textarea>
         </div>
         <div class="mb-3 col-6">
-          <input type="checkbox" class="form-check-input me-2" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">Subscribe</label>
+          <input v-model="form.subscribe" type="checkbox" class="form-check-input me-2" id="subscribe" value="1"
+            name="subscribe" />
+          <label class="form-check-label" for="subscribe">Subscribe</label>
         </div>
         <div>
           <button type="submit" class="btn btn-primary col-1">Submit</button>
